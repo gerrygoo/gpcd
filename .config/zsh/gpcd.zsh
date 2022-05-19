2f8f903a61c20e51c08bfedf6c2e08e041598709
Gerardo Galván <ggerardo@google.com>
2022-05-19 14:22:59 -0500

    correct gpcd setup scripts

--- .config/zsh/gpcd.zsh
+++ .config/zsh/gpcd.zsh
@@ -1,12 +1,12 @@
-# avoid recursion issues
-echo ".gpcd" >> .gitignore
+\# avoid recursion issues
+echo ".config" >> .gitignore
 
 # define the alias
-alias gpcd='/usr/bin/git --git-dir=$HOME/.gpcd/ --work-tree=$HOME'
+alias gpcd='git --git-dir=$HOME/.gpcd/ --work-tree=$HOME'
 
 # initialize the repo
 gpcd_clone() {
-  git clone --bare $1 $HOME/.gpcd
-	gpcd config --local status.showUntrackedFiles no
-	echo "run 'gpcd checkout' to start using gpcd"
+  git clone --bare $1 $HOME/.config
+  gpcd config --local status.showUntrackedFiles no
+  echo "run 'gpcd checkout' to start using gpcd"
 }
